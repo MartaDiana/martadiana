d60c9de8964d5d573361d00108a2ccb117191628
Marta Diana <martadiana2503@gmail.com>
2022-07-24 23:37:46 +0700

    m

--- js/main.js
+++ js/main.js
@@ -19,9 +19,7 @@ const app = Vue.createApp({
         return this.users;
       }
       return this.users.filter((user) => {
-        return Object.values(user).some((word) =>
-          String(word).toLowerCase().includes(query)
-        );
+        return Object.values(user).some((word) => String(word).toLowerCase().includes(query));
       });
     },
   },
@@ -80,18 +78,19 @@ const app = Vue.createApp({
       console.log(data);
     },
 
-    /*
     async save() {
       console.log(1);
       const response = await axios.post(
         "https://gorest.co.in/public/v2/posts",
         {
-          title: "test title",
+          user: this.users[0].name,
+          user_id: this.users[0].id,
+          title: "test title", // change this to get value from form
+          body: "test body", // change this to get value form form
         },
         {
           headers: {
-            Authorization:
-              "Bearer d7666eb7b6d38314bb70a670b27047a3d80c9d8dac93bce2ee1a9bad2c608b14",
+            Authorization: "Bearer d7666eb7b6d38314bb70a670b27047a3d80c9d8dac93bce2ee1a9bad2c608b14",
           },
         }
       );
@@ -100,7 +99,8 @@ const app = Vue.createApp({
       console.log(data);
       this.form.title = "";
       this.form.body = "";
-    },*/
+      this.post = [data, this.post];
+    },
   },
 });
 
